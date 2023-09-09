10bf2c350993f708ad1801ba481f01317cc205f2
Marina Vendrell <84765188+marinavendrell@users.noreply.github.com>
2023-09-09 21:33:06 +0200

    Update app_streamlit_produccion.py

--- app_streamlit_produccion.py
+++ app_streamlit_produccion.py
@@ -1247,7 +1247,7 @@ else:
     st.write('It has the potential to optimise the management and planning of power generation in solar fields, helping the company to maximise its operational efficiency and reduce costs.')
     st.write('The application contains an artificial intelligence model designed exclusively for the **Helechal solar field (ES)**.  Therefore, the information uploaded and the predictions will be made only for this field.')
     st.write(f"To proceed, please upload the excel file in the left sidebar. These must contain the solar field information detailed from **{fecha_24_horas_antes.strftime('%d-%m-%Y at %Hh')}** to **{fecha_hora_actual.strftime('%d-%m-%Y at %Hh')}**, all period included otherwise the application will not work.")
-    st.write('The variables required by the user to run the model are the following:')
+    st.write('The variables required by the user to upload and therefore to run the model are the following:')
     st.markdown("<b><em>Irradiation_average</b></em>, <b><em>Power by Inverter</b></em>, <b><em>Ambient Temperature</b></em>, <b><em>Module Temperature</b></em>, <b><em>Soiling Loss Sensor 1</b></em> and <b><em>Soiling Loss Sensor 2</b></em> ", unsafe_allow_html=True)
     
     st.write('* Please note that due to the fact that this is a demo application, weather data is accessed via a free account. The forecast can only be calculated approximately 35 times per day. If access is required more than this, the account will be blocked and the application will not work. In the future, a premium account will probably be created to avoid this effect.')
